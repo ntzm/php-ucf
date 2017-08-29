bfead7f547f55cdc45b21dc1bd39a91b0a2d54bb
Nat Zimmermann <nat@razorcreations.com>
2017-08-29 13:32:12 +0100

    Style

--- tests/Comment/TypeDetectorTest.php
+++ tests/Comment/TypeDetectorTest.php
@@ -77,7 +77,7 @@ final class TypeDetectorTest extends TestCase
             [
                 '/**
                   * foo
-                  */'
+                  */',
             ],
         ];
     }
