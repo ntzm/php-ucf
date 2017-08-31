16965b53654870e4e3bda2af80c3864ed5d6233c
Nat Zimmermann <nat@razorcreations.com>
2017-08-31 12:49:31 +0100

    Add ShortCommentClassifier tests

---
+++ tests/Classifier/ShortCommentClassifierTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Ntzm\Tests\PhpUcf\Classifier;
+
+use Ntzm\PhpUcf\Classifier\ShortCommentClassifier;
+use Ntzm\PhpUcf\Comment\Comment;
+use PHPUnit\Framework\TestCase;
+
+final class ShortCommentClassifierTest extends TestCase
+{
+	/**
+	 * @param string $comment
+	 *
+	 * @dataProvider provideShortComments
+	 */
+	public function testShortComments(string $comment): void
+	{
+		$comment = new Comment($comment, 1, Comment::TYPE_SINGLE_LINE);
+
+		$this->assertTrue((new ShortCommentClassifier())->isUseless($comment));
+	}
+
+	public function provideShortComments(): array
+	{
+		return [
+			['if thing'],
+			['foo bar'],
+			[''],
+			['foo'],
+		];
+	}
+
+	/**
+	 * @param string $comment
+	 *
+	 * @dataProvider provideLongComments
+	 */
+	public function testLongComments(string $comment): void
+	{
+		$comment = new Comment($comment, 1, Comment::TYPE_SINGLE_LINE);
+
+		$this->assertNull((new ShortCommentClassifier())->isUseless($comment));
+	}
+
+	public function provideLongComments(): array
+	{
+		return [
+			['if thing is thingy'],
+			['foo bar baz'],
+		];
+	}
+}
