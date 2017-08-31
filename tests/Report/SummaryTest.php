023544accc5ccb0a3760ed61004b536ee222ebfd
Nat Zimmermann <nat@razorcreations.com>
2017-08-31 12:53:11 +0100

    Add summary test

---
+++ tests/Report/SummaryTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Ntzm\Tests\PhpUcf\Comment;
+
+use Ntzm\PhpUcf\Report\Summary;
+use PHPUnit\Framework\TestCase;
+
+final class SummaryTest extends TestCase
+{
+	public function testGetTimeInMilliseconds()
+	{
+		$summary = new Summary([], 100, 0);
+
+		$this->assertEquals(100, $summary->getTimeInMilliseconds());
+	}
+
+	public function testGetTimeInSeconds()
+	{
+		$summary = new Summary([], 10000, 0);
+
+		$this->assertEquals(10, $summary->getTimeInSeconds());
+	}
+
+	public function testGetMemoryInBytes()
+	{
+		$summary = new Summary([], 0, 100);
+
+		$this->assertEquals(100, $summary->getMemoryInBytes());
+	}
+
+	public function testGetMemoryInMegabytes()
+	{
+		$summary = new Summary([], 0, 3145728);
+
+		$this->assertEquals(3, $summary->getMemoryInMegabytes());
+	}
+}
